afca947dd94903236d04b16af20e550e7e30c9ad
fahadrashid92 <55449188+fahadrashid92@users.noreply.github.com>
2020-10-12 10:35:44 +0400

    Add files via upload

---
+++ forms/sendemail.php
@@ -0,0 +1,71 @@
+<?php
+  
+    require ("class.phpmailer.php");
+  
+    if(isset($_POST['submit'])){
+        $name=$_POST['name']; // Get Name value from HTML Form
+        $email=$_POST['email'];  // Get Email Value
+        $message=$_POST['message']; // Get Message Value
+          
+          
+        $mail = new PHPMailer();
+          
+        $mail->IsSMTP();
+        $mail->Host = "gmail.com"; // Your Domain Name
+          
+        $mail->SMTPAuth = true;
+        $mail->Port = 587;
+        $mail->Username = "giftmywish@gmail.com"; // Your Email ID
+        $mail->Password = "Mel@1386"; // Password of your email id
+          
+        $mail->From = "giftmywish@gmail.com";
+        $mail->FromName = "Neo Solutions Team";
+        $mail->AddAddress ("giftmywish@gmail.com"); // On which email id you want to get the message
+        $mail->AddCC ($email);
+          
+        $mail->IsHTML(true);
+          
+        $mail->Subject = "Enquiry from Website submitted by $name"; // This is your subject
+          
+        // HTML Message Starts here
+          
+        $mail->Body = "
+        <html>
+            <body>
+                <table style='width:600px;'>
+                    <tbody>
+                        <tr>
+                            <td style='width:150px'><strong>Name: </strong></td>
+                            <td style='width:400px'>$name</td>
+                        </tr>
+                        <tr>
+                            <td style='width:150px'><strong>Email ID: </strong></td>
+                            <td style='width:400px'>$email</td>
+                        </tr>
+                        <tr>
+                            <td style='width:150px'><strong>Message: </strong></td>
+                            <td style='width:400px'>$message</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </body>
+        </html>
+        ";
+        // HTML Message Ends here
+          
+              
+        if(!$mail->Send()) {
+            // Message if mail has been sent
+            echo "<script>
+                alert('Submission failed.');
+            </script>";
+        }
+        else {
+            // Message if mail has been not sent
+            echo "<script>
+                alert('Email has been sent successfully.');
+            </script>";
+        }
+  
+    }
+?>
