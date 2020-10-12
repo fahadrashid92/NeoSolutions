f23fd060ea35afaa389aa8ee1abd8e5a7816b522
fahadrashid92 <55449188+fahadrashid92@users.noreply.github.com>
2020-10-12 11:45:22 +0400

    Update sendemail.php

--- forms/sendemail.php
+++ forms/sendemail.php
@@ -15,7 +15,7 @@ try {
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = gethostbyname('smtp.gmail.com');  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'giftmywish@gmail.com';                 // SMTP username
+    $mail->UserName = 'giftmywish@gmail.com';                 // SMTP username
     $mail->Password = 'Mel@1386';                           // SMTP password
     $mail->SMTPSecure = 'tls';                           // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
